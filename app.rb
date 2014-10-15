da579e9a95d19080ab82c1882755ca4389e3b6a5
Evan Turner <evturn@gmail.com>
2014-10-15 16:56:08 -0400

    everything works up until the delete. patch hasn't been attempted

--- app.rb
+++ app.rb
@@ -17,144 +17,157 @@ ActiveRecord::Base.establish_connection({
 
 get '/' do
 
-	erb :'order/index'
+	erb :'orders/index'
+end
+
+post '/parties' do
+	@party = Party.create(params[:party])
+
+  redirect "/parties/#{@party.id}"
+end
+
+get '/parties/:id' do
+	@party = Party.find(params[:id])
+	@food = Food.all
+
+	erb :'parties/show'
 end
 
 post '/orders' do
-	# get the party id from params and find the party 
-	# if params {id: 3} then params[:id]
-	# if params {party_id: 3} then params[:party_id]
- #  if params {party: {id: 3}, food: { id: 2}}
- #  params[:party][:id]
- # get the food id from params and then find the food 
-	@party = Party.find_or_create_by(params[:party_id])
+	@order = Order.create(params)
 
-
-  redirect "/orders/#{@party.id}"
+	redirect "/orders/#{@order.id}"
 end
 
 get '/orders/:id' do
 	@order = Order.find(params[:id])
-	@order_food = @order.food
-	@order_party = @order.party
 
-	erb :'order/show'
-end
-
-get '/orders/:id/edit' do
-	@order = Order.find(params[:id])
-	@order_food = @order.food
-	@order_party = @order.party
-
-	erb :'order/edit'
-end
-
-patch '/orders/:id' do
-	@order = Order.find(params[:id])
-	@order.food
-	@order.party
-	@order.food.update(params[:food])
-	@order.party.update(params[:party])
-	@order.save
-	redirect "/orders/#{@order.id}"
+	erb :'orders/show'
 end
 
 delete '/orders/:id' do
-	Order.destroy(params[:id])
-	redirect '/'
+	Order.delete(params[:id])
+	
+
+	redirect "/parties/#{@party.id}"
 end
+
+
+# get '/orders/:id/edit' do
+# 	@order = Order.find(params[:id])
+# 	@order_food = @order.food
+# 	@order_party = @order.party
+
+# 	erb :'orders/edit'
+# end
+
+# patch '/orders/:id' do
+# 	@order = Order.find(params[:id])
+# 	@order.food
+# 	@order.party
+# 	@order.food.update(params[:food])
+# 	@order.party.update(params[:party])
+# 	@order.save
+# 	redirect "/orders/#{@order.id}"
+# end
+
+# delete '/orders/:id' do
+# 	Order.destroy(params[:id])
+# 	redirect '/'
+# end
+
 
 
 
 # ------Food-------
 
-get '/foods' do
-	@foods = Food.all
-	erb :'food/index'
-end
+# get '/foods' do
+# 	@foods = Food.all
+# 	erb :'food/index'
+# end
 
-get "/foods/:id/new" do
+# get "/foods/:id/new" do
 
-	erb :'food/new'
-end
+# 	erb :'food/new'
+# end
 
-get '/foods/:id' do
-	@food = Food.find(params[:id])
-	@food_parties = @food.parties
+# get '/foods/:id' do
+# 	@food = Food.find(params[:id])
+# 	@food_parties = @food.parties
 
-	erb :'food/show'
-end
+# 	erb :'food/show'
+# end
 
-post '/foods' do
-	food = Food.create(params[:food])
-	redirect '/orders'
-end
+# post '/foods' do
+# 	food = Food.create(params[:food])
+# 	redirect '/orders'
+# end
 
-get '/foods/:id/edit' do
-	@food = Food.find(params[:id])
-	erb :'food/edit'
-end
+# get '/foods/:id/edit' do
+# 	@food = Food.find(params[:id])
+# 	erb :'food/edit'
+# end
 
-patch '/foods/:id' do
-	food = Food.find(params[:id])
-	food.update(params[:food])
-	food.save
-	redirect '/foods'
-end
+# patch '/foods/:id' do
+# 	food = Food.find(params[:id])
+# 	food.update(params[:food])
+# 	food.save
+# 	redirect '/foods'
+# end
 
-delete '/foods/:id' do
-	Food.destroy(params[:id])
-	redirect '/foods'
-end
+# delete '/foods/:id' do
+# 	Food.destroy(params[:id])
+# 	redirect '/foods'
+# end
 
-#
-# ------Party-------
-#
-#
-#
-#
-#
-#
-#
+# #
+# # ------Party-------
+# #
+# #
+# #
+# #
+# #
+# #
+# #
 
-get '/parties' do
-	@parties = Party.all
+# get '/parties' do
+# 	@parties = Party.all
 
-	erb :'party/index'
-end
+# 	erb :'party/index'
+# end
 
-get '/parties/new' do
+# get '/parties/new' do
 
-	erb :'party/new'
-end
+# 	erb :'party/new'
+# end
 
-get '/parties/:id' do
-	@party = Party.find(params[:id])
-	@party_foods = @party.foods
-	erb :'party/show'
-end
+# get '/parties/:id' do
+# 	@party = Party.find(params[:id])
+# 	@party_foods = @party.foods
+# 	erb :'party/show'
+# end
 
-post '/parties' do
-	party = Party.create(params[:party])
-	redirect "/foods/#{party.id}/new"
-end
+# post '/parties' do
+# 	party = Party.create(params[:party])
+# 	redirect "/foods/#{party.id}/new"
+# end
 
-get '/parties/:id/edit' do
-	@party = Party.find(params[:id])
-	erb :'party/edit'
-end
+# get '/parties/:id/edit' do
+# 	@party = Party.find(params[:id])
+# 	erb :'party/edit'
+# end
 
-patch '/parties/:id' do
-	party = Party.find(params[:id])
-	party.update(params[:party])
-	party.save
-	redirect '/parties'
-end
+# patch '/parties/:id' do
+# 	party = Party.find(params[:id])
+# 	party.update(params[:party])
+# 	party.save
+# 	redirect '/parties'
+# end
 
-delete '/parties/:id' do
-	Party.destroy(params[:id])
-	redirect '/parties'
-end
+# delete '/parties/:id' do
+# 	Party.destroy(params[:id])
+# 	redirect '/parties'
+# end
 
 
 
