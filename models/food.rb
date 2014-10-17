ebc110a56057a92a939d7cdd3472817ab603910e
Evan <evturn>
2014-10-17 10:04:03 -0400

    Delete button removes a single item

--- models/food.rb
+++ models/food.rb
@@ -2,7 +2,9 @@ class Food < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 	has_many(:orders)
 	has_many(:parties, :through => :orders)
+	
 	def to_s
     name? ? "Item: #{name}" : "You haven't ordered."
   end
+
 end
